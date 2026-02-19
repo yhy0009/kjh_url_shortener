01f70f30708908c7e687ba3c9b45f949cae064ad
yhy0009 <ref0009@naver.com>
2026-02-19 15:18:02 +0900

    fix : 깃허브 oicd 권한 내역 수정

--- Terraform/modules/iam_github_oidc/main.tf
+++ Terraform/modules/iam_github_oidc/main.tf
@@ -5,7 +5,8 @@ resource "aws_iam_openid_connect_provider" "github" {
   client_id_list = ["sts.amazonaws.com"]
 
   thumbprint_list = [
-    "6938fd4d98bab03faadb97b34396831e3780aea1"
+    "6938fd4d98bab03faadb97b34396831e3780aea1",
+    "1c58a3a8518e8759bf075b76b750d4f2df264fcd"
   ]
 }
 
@@ -29,7 +30,7 @@ data "aws_iam_policy_document" "assume_role" {
     condition {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
-      values   = ["repo:${var.repo}:ref:refs/heads/main"]
+      values = ["repo:${var.repo}:*"]
     }
   }
 }
